325d1d890c5cbc9a0fc0dc6cd089a93f1f8d4ab4
Stuart Warren <stuart.warren@uts.edu.au>
2018-09-19 04:58:57 +1000

    Tidy up.

--- 1 - hello/hello.py
+++ 1 - hello/hello.py
@@ -3,31 +3,35 @@
 # This is how we load a library in Python
 from ev3dev2.sound import Sound
 
-# By the way, this is a comment!
+# By the way, this is a comment! Comments are often used to explain how the code
+# works. There is some code below that you can uncomment by removing the "#" 
+# characters.
 
 # "Sound" is a "class". It's a type of thing.
-# We're now going to create an "instance"
+# We're now going to create an "instance" (or a thing).
 sound = Sound()
 
 # And now we're going to call a "method" on the instance.
-# What do you think this does?
+# What do you think this does? Can you make it say your name?
+# Hint: Press the "F5" key in Visual Studio Code to run the program.
 sound.speak('Hello there')
 
 #
 # What other things would you expect to do with a sound instance?
+# How about:
 #
 
-## Set the volume (100 is the loudest)
-sound.set_volume(50)
+## .. set the volume (100 is the loudest)  
+#sound.set_volume(50)
 
-## play a beep?
-# sound.beep()
+## ... play a beep?
+#sound.beep()
 
-## play a sound from a file
-sound.play('elephant.wav')
+## ... play a sound from a file (can you see another sound file on the left?)
+#sound.play('elephant.wav')
 
-## Play a song?
-# sound.play_song((
+## ... play a song? All of the lines below need to be uncommented.
+#sound.play_song((
 #    ('A2', 'q'), 
 #    ('A2', 'q'),
 #    ('A2', 'q'),
@@ -37,4 +41,3 @@ sound.play('elephant.wav')
 #    ('F2', 'q/2'),
 #    ('C3', 'q/3'),
 #    ('A2', 'q')))
-
