5255469a70ae746b9a361de2e4a60885f792f587
Stuart Warren <stuart.warren@uts.edu.au>
2018-09-19 06:14:11 +1000

    Tidied up activity.

--- 4 - maze_runner/maze_runner.py
+++ 4 - maze_runner/maze_runner.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+#
+# Abstract MoveTank so we can use a higher-level interface
+#
+
 import os
 import time
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent, MoveTank
+from ev3dev2.motor import *
 
-tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
 
 # There's a lot to remember when using tank_drive. Let's make our lives easier
 # by defining some functions that are useful for driving through a maze.
@@ -14,7 +18,13 @@ tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
 # functions below...
 
 # lets start with defining the number of cm per revolution. What did you measure
+# in exercise 1 in the last activity?
+
 CM_PER_REV = 1.0
+
+# Here are some variables that might be useful later on.
+STRAIGHT_SPEED = 90
+TURNING_SPEED = 20
 
 def forward(distance):
     '''distance: in cm'''
@@ -25,6 +35,8 @@ def forward(distance):
         distance/CM_PER_REV # number of revolutions (of the fastest wheel)
     )
 
+# In exercise 5 you worked out how to make the motors go backwards. Change the 
+# parameters in the code below to make the robot goes backwards.
 def backward(distance):
     '''distance: in cm'''
     tank_drive.on_for_rotations(
@@ -33,6 +45,8 @@ def backward(distance):
         distance/CM_PER_REV # number of revolutions (of the fastest wheel)
     )
 
+# In execrise 6 you worked out the parameters to turn 90 degrees left. Change
+# the parameters in the code below to turn left.
 def left(angle=90):
     tank_drive.on_for_rotations(
         SpeedPercent(20),   # left motor speed
@@ -40,6 +54,8 @@ def left(angle=90):
         1.0                 # number of revolutions (of the fastest wheel)
     )
 
+# In execrise 7 you worked out the parameters to turn 90 degrees left. Change
+# the parameters in the code below to turn left.
 def right(angle=90):
     tank_drive.on_for_rotations(
         SpeedPercent(20),   # left motor speed
@@ -47,7 +63,7 @@ def right(angle=90):
         1.0                 # number of revolutions (of the fastest wheel)
     )
 
-# What whape would the robot trace out with these instructions?
+# What shape would the robot trace out with these instructions?
 forward(10)
 left()
 forward(10)
@@ -55,3 +71,7 @@ left()
 forward(10)
 left()
 forward(10)
+
+# Try again with a different speed. You could try using the STRAIGHT_SPEED
+# and TURNING_SPEED variables to save you changing the same number in many
+# places.
