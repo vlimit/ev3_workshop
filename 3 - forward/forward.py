143fd69517b97e7c4c8c5770601c54e7fa02877c
Stuart Warren <stuart.warren@uts.edu.au>
2018-09-19 05:33:19 +1000

    Corrected motor ports and added handout.

--- 3 - forward/forward.py
+++ 3 - forward/forward.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python3
 
+#
+# Introduction to tank drive. Work out the parameters to go forward, backward,
+# left & right.
+#
+
 import os
 import time
 
 # Motors! This is getting interesting now :).
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent, MoveTank
+from ev3dev2.motor import *
 
 # Guess what this does. How does a tank work? And what are your motors connected to?
-tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
 
 # Let's drive the robot (I mean tank):
 tank_drive.on_for_rotations(
     SpeedPercent(20),   # left motor speed
-    SpeedPercent(-20),  # right motor speed
-    1.0                 # number of revolutions (of the fastest wheel)
+    SpeedPercent(20),   # right motor speed
+    1.0                 # number of wheel revolutions (of the fastest wheel)
 )
 
-# Exercises (write down all the parameters you used)
+# Exercises (write down all the parameters you used on the paper handout)
 
 # 1. How far (cm) does it go in one wheel revolution?
 
-# 2. Can you make it go faster?
+# 2. How wide is a wheel?
 
-# 3. Can you make it go backwards?
+# 3. How far apart are the wheels?
 
-# 4. Can you make it turn 90 degrees left?
+# 4. Can you make it go faster?
 
-# 5. Can you make it turn 90 degrees right?
+# 5. Can you make it go backwards?
 
-# 6. How wide is a wheel?
+# 6. Can you make it turn 90 degrees left?
 
-# 7. How far apart are the wheels?
-
+# 7. Can you make it turn 90 degrees right?
